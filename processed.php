d66e40b8fb87aefe2d099d1a70b905c3280ef377
farkhan7771 <farhanhamzah71@gmail.com>
2021-03-15 11:42:14 +0700

    first commit

---
+++ processed.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <h1>Thank You ^_^</h1>
+    </div>
+    <?php 
+
+    if(isset($_POST['send']))
+    {
+       $UserName = $_POST['name'];
+       $Email = $_POST['email'];
+       $Subject = $_POST['subject'];
+       $Msg = $_POST['message'];
+
+       if(empty($UserName) || empty($Email) || empty($Subject) || empty($Msg))
+       {
+           header('location:index.php?error');
+       }
+       else
+       {
+           $to = "farhanhamzah71@gmail.com";
+
+           if(mail($to,$Subject,$Msg,$Email))
+           {
+               header("location:index.php?success");
+           }
+       }
+    }
+    else
+    {
+        header("location:index.php");
+    }
+?>
+</body>
+</html>
+
