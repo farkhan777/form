a1476560fb41bf582c41137db3ac163533bfb81b
farkhan7771 <farhanhamzah71@gmail.com>
2021-03-15 15:29:48 +0700

    creating a new page

--- mailsent.php
+++ mailsent.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -29,37 +30,27 @@
 				<img src="images/message.png" alt="IMG">
 			</div>
 
-			<form class="contact1-form validate-form" action="processed.php" method="post">
-				<span class="contact1-form-title">
-					Get in touch
-				</span>
-				<div class="wrap-input1 validate-input" data-validate = "Name is required">
-					<input class="input1" type="text" name="name" placeholder="Name">
-					<span class="shadow-input1"></span>
-				</div>
+            <?php
+                $Msg = "";
+                if(isset($_GET['error']))
+	            {
+                    $Msg = " Please Fill in the Blanks ";
+		            echo '<div class="alert alert-danger text-center">'.$Msg.'</div>';
+                }
 
-				<div class="wrap-input1 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
-					<input class="input1" type="text" name="email" placeholder="Email">
-					<span class="shadow-input1"></span>
-				</div>
-
-				<div class="wrap-input1 validate-input" data-validate = "Subject is required">
-					<input class="input1" type="text" name="subject" placeholder="Subject">
-					<span class="shadow-input1"></span>
-				</div>
-
-				<div class="wrap-input1 validate-input" data-validate = "Message is required">
-					<textarea class="input1" name="message" placeholder="Message"></textarea>
-					<span class="shadow-input1"></span>
-				</div>
-
-				<div class="container-contact1-form-btn">
-					<button class="contact1-form-btn" name="send">
-							Send Email
-							<i class="fa fa-long-arrow-right" aria-hidden="true"></i>
-					</button>
-				</div>
-			</form>
+                if(isset($_GET['success']))
+                {
+                    $Msg = " Your Message Has Been Sent ";
+		            echo '<div class="alert alert-success text-center">'.$Msg.'</div>';
+                }
+            ?>
+		</div>
+        <div class="container-contact2-form-btn">
+            <a href="https://farkhan-312.com">
+                <button class="contact2-form-btn">
+                    <i class="fa fa-long-arrow-left" aria-hidden="true"> Go Back</i>
+                </button>
+            </a>
 		</div>
 	</div>
 
--- processed.php
+++ processed.php
@@ -6,13 +6,19 @@ if(isset($_POST['send']))
     $Email = $_POST['email'];
     $Subject = $_POST['subject'];
     $Msg = $_POST['message'];
-    
-    $mailTo = "farhanhamzah91@gmail.com";
-    $headers = "From: ".$Email;
-    $txt = "You have received an e-mail from ".$UserName.".\n\n".$Msg;
 
+    if(empty($UserName) || empty($Email) || empty($Subject) || empty($Msg))
+    {
+        header('location:mailsent.php?error');
+    }
+    else
+    {
+        $mailTo = "farhanhamzah91@gmail.com";
+        $headers = "From: ".$Email;
+        $txt = "You have received an e-mail from ".$UserName.".\n\n".$Msg;
 
-    mail($mailTo, $Subject, $txt, $headers);
-    header("Location: index.php?mailsend");
+        mail($mailTo, $Subject, $txt, $headers);
+        header("location:mailsent.php?success");
+    }
 }
 ?>
